6e1780a1e225d001f022f5e77342448f524b0689
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-22 13:12:46 +0000

    Upgrade to inspircd 3.

--- code/kubes/ircd/inspircd/init.sh
+++ code/kubes/ircd/inspircd/init.sh
@@ -4,4 +4,4 @@ while [ ! -f conf/secret/server.conf ]; do
   echo "[$(date)] Waiting for conf/secret/server.conf..."
   sleep 30
 done
-exec /usr/sbin/inspircd --config inspircd.conf --logfile /dev/stderr --nofork
+exec /usr/sbin/inspircd --config inspircd.conf --nofork
