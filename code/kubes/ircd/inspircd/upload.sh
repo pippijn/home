a278f4f0ed2d8511879e79ca33b397ade4c83c4b
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-22 23:13:13 +0000

    Automatically find the pod to upload config changes to.

--- code/kubes/ircd/inspircd/upload.sh
+++ code/kubes/ircd/inspircd/upload.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo kubectl cp inspircd/conf/secret ircd/inspircd-7f8ff897cf-c9s5h:/etc/inspircd/conf/
+set -eux
+
+POD=$(sudo kubectl get pod -n ircd | grep '^inspircd' | awk '{print $1}')
+sudo kubectl cp inspircd/conf/secret "ircd/$POD:/etc/inspircd/conf/"
+sudo kubectl exec --stdin --tty -n ircd "pod/$POD" -- /bin/bash -c 'kill -HUP 1'
