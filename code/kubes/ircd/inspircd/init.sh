cf82d064ef4d3c6878b4ea259a0f93cbf6af63be
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-22 16:08:54 +0000

    Add auto config pull and rehash to the container.

--- code/kubes/ircd/inspircd/init.sh
+++ code/kubes/ircd/inspircd/init.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
+# The first time, the secrets need to be copied into the container. After
+# that, these are persisted while the shared configs are not.
 while [ ! -f conf/secret/server.conf ]; do
   echo "[$(date)] Waiting for conf/secret/server.conf..."
   sleep 30
 done
-exec /usr/sbin/inspircd --config inspircd.conf --nofork
+
+config_pull() {
+  # Initial git ref.
+  git rev-parse HEAD > /tmp/inspircd.hash
+
+  while true; do
+    # Every 10 minutes.
+    sleep 600
+
+    echo "$(date) Checking for new configs"
+    git pull
+    # Check whether anything changed.
+    git rev-parse HEAD > /tmp/inspircd.hash.new
+    diff /tmp/inspircd.hash.new /tmp/inspircd.hash || {
+      mv /tmp/inspircd.hash.new /tmp/inspircd.hash
+      /usr/sbin/inspircd rehash
+    }
+  done
+}
+
+# Initial pull on startup.
+git pull
+# Continuous fetch and rehash loop.
+config_pull &
+exec /usr/sbin/inspircd --nofork
