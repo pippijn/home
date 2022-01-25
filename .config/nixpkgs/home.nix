7dc81b2d3a6b0d9a72c89a43af5b81aaa0c0d21f
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-25 14:44:13 +0000

    Install unison program.

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -13,6 +13,7 @@ let sys = (import <nixpkgs/nixos> {}).config; in
     gnupg
     keychain
     screen
+    unison
   ];
 
   # This value determines the Home Manager release that your
@@ -29,17 +30,17 @@ let sys = (import <nixpkgs/nixos> {}).config; in
     EDITOR = "vi";
   };
 
-  # https://github.com/nix-community/home-manager/issues/2662
-#  services.unison = {
-#    enable = sys.networking.hostName != "amun";
-#
-#    pairs = {
-#      roots = [
-#        "/home/pippijn/Photos"
-#        "ssh://amun/home/pippijn/Photos"
-#      ];
-#    };
-#  };
+  services.unison = {
+    enable = sys.networking.hostName != "amun";
+
+    # https://github.com/nix-community/home-manager/issues/2662
+#   pairs = {
+#     roots = [
+#       "/home/pippijn/Photos"
+#       "ssh://amun/home/pippijn/Photos"
+#     ];
+#   };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
