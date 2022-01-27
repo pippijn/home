75d2f1b306009c29d1b2bbccdece6b4bc812842b
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-27 17:05:55 +0000

    Fix redis host password envvar.

--- code/kubes/nextcloud/nextcloud/sync.sh
+++ code/kubes/nextcloud/nextcloud/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo helm upgrade --install redis bitnami/redis -n nextcloud --create-namespace --values redis-values.yaml
+sudo helm upgrade --install redis bitnami/redis -n nextcloud --create-namespace --values helm/redis-values.yaml
