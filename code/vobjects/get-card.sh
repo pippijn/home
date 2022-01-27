34379deb8838b4cb8f8643703b0afca5e4eeb383
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-27 17:04:47 +0000

    Update sync_mtime for new dir layout.

---
+++ code/vobjects/get-card.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+USER=pippijn
+
+#curl -u "$USER:$PASS" --insecure -i -X PROPFIND "https://dash.xinutec.org/remote.php/dav/addressbooks/users/$USER/contacts/" --upload-file - -H "Depth: 1" <<end
+#<?xml version="1.0"?>
+#<a:propfind xmlns:a="DAV:">
+#<a:prop><a:resourcetype/></a:prop>
+#</a:propfind>
+#end
+
+curl -u "$USER:$PASS" -v http://nextcloud-server.nextcloud.svc.cluster.local/remote.php/dav/addressbooks/users/pippijn/contacts/824408F2-44AB-4900-AC6F-F229B53D5C83.vcf
