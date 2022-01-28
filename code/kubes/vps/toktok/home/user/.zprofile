c878cc9ced6daf123ce5c27dd03144fb879f5c50
Pippijn van Steenhoven <pippijn@xinutec.org>
2022-01-28 00:20:59 +0000

    Add dev env.

---
+++ code/kubes/vps/toktok/home/user/.zprofile
@@ -0,0 +1,6 @@
+#!/usr/bin/zsh
+
+# Any user zsh settings go here.
+export CC=/usr/local/bin/clang
+export CXX=/usr/local/bin/clang++
+cd /src/workspace
---
+++ code/kubes/vps/toktok/init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eux
+
+service ssh start
+
+# The first time the container is started, SSH keys are generated.
+if [ ! -f /etc/ssh_keys/.stamp ]; then
+  # We move those to the persistent storage.
+  mv /etc/ssh/ssh_host_* /etc/ssh_keys/
+fi
+# Then we symlink them back. If there was a .stamp file, this is not the
+# first time and so we'll link the old persisted keys back.
+ln -sf /etc/ssh_keys/* /etc/ssh/
+touch /etc/ssh_keys/.stamp
+
+if grep "BEGIN" ~user/key.pem; then
+  sudo -i -u user gpg --import ~user/key.pem
+fi
+
+while true; do
+  sleep 3600
+done
