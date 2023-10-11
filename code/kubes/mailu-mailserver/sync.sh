155fc975020b90795dd1161e6f265ee5589d5274
Pippijn van Steenhoven <pippijn@xinutec.org>
2023-10-11 10:28:54 +0000

    Upgrade nextcloud to v26.

--- code/kubes/mailu-mailserver/sync.sh
+++ code/kubes/mailu-mailserver/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo helm upgrade --install mailu mailu/mailu -n mailu-mailserver --create-namespace --values values.yaml
+sudo helm upgrade --install mailu mailu/mailu --version 0.3.5 -n mailu-mailserver --create-namespace --values values.yaml
