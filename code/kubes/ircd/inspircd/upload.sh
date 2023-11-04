94e218a7b9d465d95107d5e7fd6df5a4cb4fa584
Pippijn van Steenhoven <pippijn@xinutec.org>
2023-11-04 14:36:54 +0000

    Update inspircd conf.

--- code/kubes/ircd/inspircd/upload.sh
+++ code/kubes/ircd/inspircd/upload.sh
@@ -2,9 +2,13 @@
 
 set -eux
 
-sudo kubectl get secret -n ircd irc-tls -o json | jq -r '.data."tls.crt"' | base64 -d > inspircd/conf/secret/cert.pem
-sudo kubectl get secret -n ircd irc-tls -o json | jq -r '.data."tls.key"' | base64 -d > inspircd/conf/secret/key.pem
-
 POD=$(sudo kubectl get pod -n ircd | grep '^inspircd' | awk '{print $1}')
-sudo kubectl cp inspircd/conf/secret "ircd/$POD:/etc/inspircd/conf/"
+HOST=org.xinutec.amun
+
+# Same for the certs (encrypted)>
+sudo kubectl get secret -n ircd irc-tls -o json | jq -r '.data."tls.crt"' | base64 -d > "inspircd/conf/$HOST/secret/cert.pem"
+sudo kubectl get secret -n ircd irc-tls -o json | jq -r '.data."tls.key"' | base64 -d > "inspircd/conf/$HOST/secret/key.pem"
+
+# Copy the certs into the container and REHASH.
+sudo kubectl cp inspircd/conf/$HOST/secret "ircd/$POD:/etc/inspircd/conf/"
 sudo kubectl exec --stdin --tty -n ircd "pod/$POD" -- /bin/bash -c 'kill -HUP 1'
