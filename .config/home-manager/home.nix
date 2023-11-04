07124a098cb3029ddf00878fd84c97ac0ea560fd
Pippijn van Steenhoven <pippijn@xinutec.org>
2023-11-04 16:32:46 +0000

    Update stateVersion.

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -28,9 +28,11 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.11";
+  home.stateVersion = "23.05";
 
-  home.sessionVariables = { EDITOR = "vi"; };
+  home.sessionVariables = {
+    EDITOR = config.programs.zsh.shellAliases.vi;
+  };
 
   programs.gpg.enable = true;
   services.gpg-agent.enable = true;
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Pippijn's Home Directory
 
 Not much to see here. These are my configs on NixOS hosts.
+
+This repo also builds some of the Kubernetes container images. See the
+Actions tab for details.
