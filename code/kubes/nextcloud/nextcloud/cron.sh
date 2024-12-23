96e1f9c6153c7196cd3d9ab1852c7e160893d931
Pippijn van Steenhoven <pippijn@xinutec.org>
2024-12-23 16:47:31 +0000

    Add nextcloud secrets.

--- code/kubes/nextcloud/nextcloud/cron.sh
+++ code/kubes/nextcloud/nextcloud/cron.sh
@@ -1,2 +1,2 @@
-sudo k3s kubectl exec --stdin --tty -n nextcloud deployment/nextcloud-server -c nextcloud -- apk add sudo
-sudo k3s kubectl exec --stdin --tty -n nextcloud deployment/nextcloud-server -c nextcloud -- sudo -u www-data php -d memory_limit=1024M ./cron.php
+sudo kubectl exec --stdin --tty -n nextcloud deployment/nextcloud-server -c nextcloud -- apk add sudo
+sudo kubectl exec --stdin --tty -n nextcloud deployment/nextcloud-server -c nextcloud -- sudo -u www-data php -d memory_limit=1024M ./cron.php
